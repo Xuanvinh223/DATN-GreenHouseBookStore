a3685e48815be4e35113ae5fdec1e0912c164544
Hothivananh <114340582+Hothivananh@users.noreply.github.com>
2023-09-01 18:40:33 +0700

    [Front-end] update menu and header index

--- font-end/client-template/js/code.js
+++ font-end/client-template/js/code.js
@@ -34,16 +34,32 @@ for (let i = 0; i < allElement.length; i++) {
     allElement[i].addEventListener("click", checkToggleLanguage);
 }
 
+
+//Menu Sách
 $("#sach-menu").mouseenter(function () {
-    $("#top-bar-category-container-right").attr("style", "visibility: visible;");
+    $("#top-bar-category-container-right-sach").attr("style", "visibility: visible;");
 }).mouseleave(function () {
-    $("#top-bar-category-container-right").attr("style", "visibility: hidden;");
+    $("#top-bar-category-container-right-sach").attr("style", "visibility: hidden;");
 });
 
-$("#top-bar-category-container-right").mouseenter(function () {
-    $("#top-bar-category-container-right").attr("style", "visibility: visible;");
+$("#top-bar-category-container-right-sach").mouseenter(function () {
+    $("#top-bar-category-container-right-sach").attr("style", "visibility: visible;");
     console.log("hello");
 }).mouseleave(function () {
-    $("#top-bar-category-container-right").attr("style", "visibility: hidden;");
+    $("#top-bar-category-container-right-sach").attr("style", "visibility: hidden;");
+    console.log("hellddo");
+});
+// Menu DỤNG CỤ HỌC SINH
+$("#dungcu-menu").mouseenter(function () {
+    $("#top-bar-category-container-right-dungcu").attr("style", "visibility: visible;");
+}).mouseleave(function () {
+    $("#top-bar-category-container-right-dungcu").attr("style", "visibility: hidden;");
+});
+
+$("#top-bar-category-container-right-dungcu").mouseenter(function () {
+    $("#top-bar-category-container-right-dungcu").attr("style", "visibility: visible;");
+    console.log("hello");
+}).mouseleave(function () {
+    $("#top-bar-category-container-right-dungcu").attr("style", "visibility: hidden;");
     console.log("hellddo");
 });
