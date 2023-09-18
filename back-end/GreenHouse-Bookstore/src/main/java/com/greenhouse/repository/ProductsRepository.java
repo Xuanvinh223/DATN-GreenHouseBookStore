d3b4322e5bf269ff6d47906752a5c386208f03fd
datmoi <93335350+datmoi@users.noreply.github.com>
2023-09-18 14:00:19 +0700

    [BackEnd] Create repository classes

---
+++ back-end/GreenHouse-Bookstore/src/main/java/com/greenhouse/repository/ProductsRepository.java
@@ -0,0 +1,8 @@
+package com.greenhouse.repository;
+
+import com.greenhouse.model.Products;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductsRepository extends JpaRepository<Products, String> {
+    // Các phương thức truy vấn tùy chỉnh nếu cần
+}
