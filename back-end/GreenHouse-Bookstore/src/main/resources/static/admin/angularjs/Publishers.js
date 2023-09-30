70d8b6b37f627aa223bce1aec40b28bdb9c40d62
ZonNull <110268408+ZonNull@users.noreply.github.com>
2023-09-30 13:27:19 +0700

    [BE] Validation form publishers and suppliers

--- back-end/GreenHouse-Bookstore/src/main/resources/static/admin/angularjs/Publishers.js
+++ back-end/GreenHouse-Bookstore/src/main/resources/static/admin/angularjs/Publishers.js
@@ -134,15 +134,104 @@ function PublishersController($scope, $location, $routeParams, $http) {
   }
 
 
-
   // Lưu thông tin nhà xuất bản
   $scope.savePublisher = function (publisherId) {
+    $scope.errorMessages = {
+      publisherId: '',
+      publisherName: '',
+      address: '',
+      email: ''
+    };
+
     var formData = new FormData();
     var fileInput = document.getElementById("fileInput");
-
     if (fileInput && fileInput.files.length > 0) {
       formData.append("image", fileInput.files[0]);
     }
+    var publisherId = $scope.editingPublisher.publisherId;
+    var publisherName = $scope.editingPublisher.publisherName;
+    var address = $scope.editingPublisher.address;
+    var email = $scope.editingPublisher.email;
+
+    // Kiểm tra bỏ trống mã nxb
+    if (!publisherId) {
+      $scope.errorMessages.publisherId = 'Vui lòng nhập mã nhà xuất bản';
+      return;
+    }
+    // Kiểm tra bỏ trống tên nxb
+    if (!publisherName) {
+      $scope.errorMessages.publisherName = 'Vui lòng nhập tên nhà xuất bản';
+      return;
+    }
+    // Kiểm tra bỏ trống nxb
+    if (!address) {
+      $scope.errorMessages.address = 'Vui lòng nhập địa chỉ nhà xuất bản';
+      return;
+    }
+    // Kiểm tra bỏ trống nxb
+    if (!email) {
+      $scope.errorMessages.email = 'Vui lòng nhập email nhà xuất bản';
+      return;
+    }
+
+    // Kiểm tra định dạng mã
+    var publisherIdRegex = /^[A-Z0-9]{4,}$/;
+    if (!publisherIdRegex.test(publisherId)) {
+      $scope.errorMessages.publisherId = 'Mã nhà xuất bản phải chứa ít nhất 4 ký tự và chỉ được điền kí tự HOA và số';
+      return;
+    }
+
+    // Kiểm tra trùng lặp publisherId trước khi thêm
+    if (!$scope.isEditing) {
+      var existingPublisher = $scope.publishers.find(function (publisher) {
+        return publisher.publisherId === $scope.editingPublisher.publisherId;
+      });
+
+      if (existingPublisher) {
+        // Gán thông báo lỗi vào $scope.errorMessages.publisherId
+        $scope.errorMessages.publisherId = `Mã Nhà xuất bản "${$scope.editingPublisher.publisherId}" đã tồn tại. Vui lòng chọn mã khác.`;
+        return; // Không tiếp tục lưu nếu có lỗi
+      }
+    }
+
+    // Kiểm tra trùng lặp publisherName trước khi thêm
+    var existingPublisherName = $scope.publishers.find(function (publisher) {
+      return (
+        publisher.publisherName === $scope.editingPublisher.publisherName &&
+        publisher.publisherId !== $scope.editingPublisher.publisherId
+      );
+    });
+    if (existingPublisherName) {
+      // Hiển thị thông báo lỗi nếu tên nhà xuất bản đã tồn tại
+      $scope.errorMessages.publisherName = `Tên Nhà xuất bản "${$scope.editingPublisher.publisherName}" đã tồn tại. Vui lòng chọn tên khác.`;
+      return; // Không tiếp tục lưu nếu có lỗi
+    }
+
+    // Kiểm tra định dạng email Gmail
+    function isGmail(email) {
+      var emailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+      return emailRegex.test(email);
+    }
+    if (!isGmail($scope.editingPublisher.email)) {
+      // Hiển thị thông báo lỗi nếu email không đúng định dạng Gmail
+      $scope.errorMessages.email = `Email "${$scope.editingPublisher.email}" không đúng định dạng Gmail. Vui lòng kiểm tra lại.`;
+      return; // Không tiếp tục lưu nếu có lỗi
+    }
+
+    // Kiểm tra trùng lặp email trước khi thêm
+    var existingEmail = $scope.publishers.find(function (publisher) {
+      return (
+        publisher.email === $scope.editingPublisher.email &&
+        publisher.publisherId !== $scope.editingPublisher.publisherId
+      );
+    });
+    if (existingEmail) {
+      // Hiển thị thông báo lỗi nếu email đã tồn tại
+      $scope.errorMessages.email = `Email "${$scope.editingPublisher.email}" đã tồn tại. Vui lòng chọn email khác.`;
+      return; // Không tiếp tục lưu nếu có lỗi
+    }
+
+
 
     formData.append(
       "publisherJson",
@@ -157,29 +246,44 @@ function PublishersController($scope, $location, $routeParams, $http) {
     );
 
     if ($scope.isEditing) {
-      var url = `${host}/${$scope.editingPublisher.publisherId}`;
-      $http
-        .put(url, formData, {
-          transformRequest: angular.identity,
-          headers: { "Content-Type": undefined },
-        })
-        .then((resp) => {
-          $scope.loadPublishers();
-          $scope.resetForm();
-          Swal.fire({
-            icon: "success",
-            title: "Thành công",
-            text: `Cập nhật nhà xuất bản ${publisherId}`,
-          });
-          $scope.clearImage(); // Xóa ảnh đại diện sau khi cập nhật
-        })
-        .catch((error) => {
-          Swal.fire({
-            icon: "error",
-            title: "Thất bại",
-            text: `Cập nhật nhà xuất bản ${publisherId} thất bại`,
-          });
-        });
+      // Sử dụng hộp thoại xác nhận từ thư viện Swal
+      Swal.fire({
+        title: 'Xác nhận cập nhật',
+        text: `Bạn có muốn cập nhật nhà xuất bản "${publisherId}" không?`,
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonText: 'Cập nhật',
+        cancelButtonText: 'Hủy',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          var url = `${host}/${$scope.editingPublisher.publisherId}`;
+          $http
+            .put(url, formData, {
+              transformRequest: angular.identity,
+              headers: { "Content-Type": undefined },
+            })
+            .then((resp) => {
+              $scope.loadPublishers();
+              $scope.resetForm();
+              Swal.fire({
+                icon: "success",
+                title: "Thành công",
+                text: `Cập nhật nhà xuất bản "${publisherId}" thành công`,
+              });
+              $scope.clearImage(); // Xóa ảnh đại diện sau khi cập nhật
+            })
+            .catch((error) => {
+              Swal.fire({
+                icon: "error",
+                title: "Thất bại",
+                text: `Cập nhật nhà xuất bản "${publisherId}" thất bại`,
+              });
+            });
+        } else {
+          // Nếu người dùng chọn Hủy, bạn có thể thực hiện hành động nào đó, hoặc không làm gì cả.
+          // Ví dụ: không thực hiện cập nhật và trở lại biểu mẫu.
+        }
+      });
     } else {
       var url = `${host}`;
       $http.post(url, formData, {
@@ -194,7 +298,7 @@ function PublishersController($scope, $location, $routeParams, $http) {
           Swal.fire({
             icon: "success",
             title: "Thành công",
-            text: `Thêm nhà xuất bản ${publisherId}`,
+            text: `Thêm nhà xuất bản "${publisherId}"`,
           });
           $scope.clearImage(); // Xóa ảnh đại diện sau khi thêm
         })
@@ -204,7 +308,7 @@ function PublishersController($scope, $location, $routeParams, $http) {
             Swal.fire({
               icon: "error",
               title: "Thất bại",
-              text: `Thêm nhà xuất bản ${publisherId} thất bại`,
+              text: `Thêm nhà xuất bản "${publisherId}" thất bại`,
             });
           }
         });
@@ -240,34 +344,45 @@ function PublishersController($scope, $location, $routeParams, $http) {
 
   // Xóa nhà xuất bản
   $scope.deletePublisher = function (publisherId) {
-    var url = `${host}/${publisherId}`;
-
-    $http
-      .delete(url)
-      .then((resp) => {
-        $scope.loadPublishers();
-        Swal.fire({
-          icon: "success",
-          title: "Thành công",
-          text: `Xóa nhà xuất bản ${publisherId} thành công`,
-        });
-      })
-      .catch((error) => {
-        if (error.status === 409) {
-          Swal.fire({
-            icon: "error",
-            title: "Thất bại",
-            text: `Nhà xuất bản mã ${key} đang được sử dụng và không thể xóa.`,
+    Swal.fire({
+      title: 'Xác nhận xóa',
+      text: `Bạn có muốn xóa nhà xuất bản "${publisherId}" không?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Xóa',
+      cancelButtonText: 'Hủy',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        var url = `${host}/${publisherId}`;
+        $http
+          .delete(url)
+          .then((resp) => {
+            $scope.loadPublishers();
+            Swal.fire({
+              icon: "success",
+              title: "Thành công",
+              text: `Xóa nhà xuất bản "${publisherId}" thành công`,
+            });
+          })
+          .catch((error) => {
+            if (error.status === 409) {
+              Swal.fire({
+                icon: "error",
+                title: "Thất bại",
+                text: `Nhà xuất bản mã "${publisherId}" đang được sử dụng và không thể xóa.`,
+              });
+            } else {
+              Swal.fire({
+                icon: "error",
+                title: "Thất bại",
+                text: `Xóa nhà xuất bản "${publisherId}" thất bại`,
+              });
+            }
           });
-        } else {
-          Swal.fire({
-            icon: "error",
-            title: "Thất bại",
-            text: `Xóa nhà xuất bản ${publisherId} thất bại`,
-          });
-        }
-      });
+      }
+    });
   };
+
 
   // Xóa ảnh đại diện và làm mới form
   $scope.clearImage = function () {
--- back-end/GreenHouse-Bookstore/src/main/resources/static/admin/angularjs/Suppliers.js
+++ back-end/GreenHouse-Bookstore/src/main/resources/static/admin/angularjs/Suppliers.js
@@ -7,7 +7,7 @@ function SuppliersController($scope, $location, $routeParams, $http) {
         $scope.page.setTitle(current.$$route.title || ' Quản Lý nhà cung cấp');
     });
 
-    $scope.editingPublisher = {};
+    $scope.editingSupplier = {};
     $scope.isEditing = false;
     $scope.suppliers = [];
 
@@ -17,7 +17,7 @@ function SuppliersController($scope, $location, $routeParams, $http) {
     $scope.itemsPerPageOptions = [5, 12, 24, 32, 64, 128];
     $scope.selectedItemsPerPage = 5; // Khởi tạo giá trị mặc định cho số mục trên mỗi trang
 
-    // Load danh sách nhà xuất bản
+    // Load danh sách nhà cung cấp
     $scope.loadSuppliers = function () {
         var url = `${host}`;
         $http.get(url)
@@ -134,14 +134,134 @@ function SuppliersController($scope, $location, $routeParams, $http) {
             });
     }
 
-    // Lưu thông tin nhà xuất bản
+    // Lưu thông tin nhà cung cấp
     $scope.saveSupplier = function (supplierId) {
+        $scope.errorMessages = {
+            supplierId: '',
+            supplierName: '',
+            address: '',
+            email: '',
+            phone: ''
+        };
         var formData = new FormData();
         var fileInput = document.getElementById("fileInput");
-
         if (fileInput && fileInput.files.length > 0) {
             formData.append("image", fileInput.files[0]);
         }
+        var supplierId = $scope.editingSupplier.supplierId;
+        var supplierName = $scope.editingSupplier.supplierName;
+        var address = $scope.editingSupplier.address;
+        var email = $scope.editingSupplier.email;
+        var phone = $scope.editingSupplier.phone;
+
+        // Kiểm tra bỏ trống mã nxb
+        if (!supplierId) {
+            $scope.errorMessages.supplierId = 'Vui lòng nhập mã nhà cung cấp';
+            return;
+        }
+        // Kiểm tra bỏ trống tên nxb
+        if (!supplierName) {
+            $scope.errorMessages.supplierName = 'Vui lòng nhập tên nhà cung cấp';
+            return;
+        }
+        // Kiểm tra bỏ trống nxb
+        if (!address) {
+            $scope.errorMessages.address = 'Vui lòng nhập địa chỉ nhà cung cấp';
+            return;
+        }
+        // Kiểm tra bỏ trống nxb
+        if (!email) {
+            $scope.errorMessages.email = 'Vui lòng nhập email nhà cung cấp';
+            return;
+        }
+        // Kiểm tra bỏ trống nxb
+        if (!phone) {
+            $scope.errorMessages.phone = 'Vui lòng nhập số điện thoại nhà cung cấp';
+            return;
+        }
+
+        // Kiểm tra định dạng mã
+        var supllierIdRegex = /^[A-Z0-9]{4,}$/;
+        if (!supllierIdRegex.test(supplierId)) {
+            $scope.errorMessages.supplierId = 'Mã nhà cung cấp phải chứa ít nhất 4 ký tự và chỉ được điền kí tự HOA và số';
+            return;
+        }
+
+        // Kiểm tra trùng lặp supplierId trước khi thêm
+        if (!$scope.isEditing) {
+            var existingSupplier = $scope.suppliers.find(function (supplier) {
+                return supplier.supplierId === $scope.editingSupplier.supplierId;
+            });
+
+            if (existingSupplier) {
+                // Gán thông báo lỗi vào $scope.errorMessages.supplierId
+                $scope.errorMessages.supplierId = `Mã Nhà cung cấp "${$scope.editingSupplier.supplierId}" đã tồn tại. Vui lòng chọn mã khác.`;
+                return; // Không tiếp tục lưu nếu có lỗi
+            }
+        }
+
+        // Kiểm tra trùng lặp supplierName trước khi thêm
+        var existingSupplierName = $scope.suppliers.find(function (supplier) {
+            return (
+                supplier.supplierName === $scope.editingSupplier.supplierName &&
+                supplier.supplierId !== $scope.editingSupplier.supplierId
+            );
+        });
+        if (existingSupplierName) {
+            // Hiển thị thông báo lỗi nếu tên nhà cung cấp đã tồn tại
+            $scope.errorMessages.supplierName = `Tên Nhà cung cấp "${$scope.editingSupplier.supplierName}" đã tồn tại. Vui lòng chọn tên khác.`;
+            return; // Không tiếp tục lưu nếu có lỗi
+        }
+
+        // Kiểm tra định dạng email Gmail
+        function isGmail(email) {
+            var emailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+            return emailRegex.test(email);
+        }
+        if (!isGmail($scope.editingSupplier.email)) {
+            // Hiển thị thông báo lỗi nếu email không đúng định dạng Gmail
+            $scope.errorMessages.email = `Email "${$scope.editingSupplier.email}" không đúng định dạng Gmail. Vui lòng kiểm tra lại.`;
+            return; // Không tiếp tục lưu nếu có lỗi
+        }
+        // Kiểm tra trùng lặp email trước khi thêm
+        var existingEmail = $scope.suppliers.find(function (supplier) {
+            return (
+                supplier.email === $scope.editingSupplier.email &&
+                supplier.supplierId !== $scope.editingSupplier.supplierId
+            );
+        });
+        if (existingEmail) {
+            // Hiển thị thông báo lỗi nếu email đã tồn tại
+            $scope.errorMessages.email = `Email "${$scope.editingSupplier.email}" đã tồn tại. Vui lòng chọn email khác.`;
+            return; // Không tiếp tục lưu nếu có lỗi
+        }
+
+        //kiểm tra định dạng sđt
+        function isPhoneNumber(phone) {
+            var phoneNumberRegex = /^(032|033|034|035|036|037|038|039|081|082|083|084|085|070|079|077|076|078|056|058)\d{7}$|^(0282|0242|0286|0246|0283|0243|0244|0245|0246|0247|0248|0287|0247|0289|0249)\d{6}$/;
+            return phoneNumberRegex.test(phone);
+        }
+
+        if (!isPhoneNumber($scope.editingSupplier.phone)) {
+            // Hiển thị thông báo lỗi nếu số điện thoại không đúng định dạng
+            $scope.errorMessages.phone = `Số điện thoại "${$scope.editingSupplier.phone}" không đúng định dạng đầu số Việt Nam. Vui lòng kiểm tra lại.`;
+            return; // Không tiếp tục lưu nếu có lỗi
+        }
+
+        // Kiểm tra trùng lặp số điện thoại trước khi thêm
+        var existingPhoneNumber = $scope.suppliers.find(function (supplier) {
+            return (
+                supplier.phone === $scope.editingSupplier.phone &&
+                supplier.supplierId !== $scope.editingSupplier.supplierId
+            );
+        });
+
+        if (existingPhoneNumber) {
+            // Hiển thị thông báo lỗi nếu số điện thoại đã tồn tại
+            $scope.errorMessages.phone = `Số điện thoại "${$scope.editingSupplier.phone}" đã tồn tại. Vui lòng chọn số điện thoại khác.`;
+            return; // Không tiếp tục lưu nếu có lỗi
+        }
+
 
         formData.append(
             "supplierJson",
@@ -157,29 +277,44 @@ function SuppliersController($scope, $location, $routeParams, $http) {
         );
 
         if ($scope.isEditing) {
-            var url = `${host}/${$scope.editingSupplier.supplierId}`;
-            $http
-                .put(url, formData, {
-                    transformRequest: angular.identity,
-                    headers: { "Content-Type": undefined },
-                })
-                .then((resp) => {
-                    $scope.loadSuppliers();
-                    $scope.resetForm();
-                    Swal.fire({
-                        icon: "success",
-                        title: "Thành công",
-                        text: `Cập nhật nhà cung cấp ${supplierId}`,
-                    });
-                    $scope.clearImage(); // Xóa ảnh đại diện sau khi cập nhật
-                })
-                .catch((error) => {
-                    Swal.fire({
-                        icon: "error",
-                        title: "Thất bại",
-                        text: `Cập nhật nhà cung cấp ${supplierId} thất bại`,
-                    });
-                });
+            // Sử dụng hộp thoại xác nhận từ thư viện Swal
+            Swal.fire({
+                title: 'Xác nhận cập nhật',
+                text: `Bạn có muốn cập nhật nhà cung cấp "${$scope.editingSupplier.supplierId}" không?`,
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonText: 'Cập nhật',
+                cancelButtonText: 'Hủy',
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    var url = `${host}/${$scope.editingSupplier.supplierId}`;
+                    $http
+                        .put(url, formData, {
+                            transformRequest: angular.identity,
+                            headers: { "Content-Type": undefined },
+                        })
+                        .then((resp) => {
+                            $scope.loadSuppliers();
+                            $scope.resetForm();
+                            Swal.fire({
+                                icon: "success",
+                                title: "Thành công",
+                                text: `Cập nhật nhà cung cấp "${$scope.editingSupplier.supplierId}" thành công`,
+                            });
+                            $scope.clearImage(); // Xóa ảnh đại diện sau khi cập nhật
+                        })
+                        .catch((error) => {
+                            Swal.fire({
+                                icon: "error",
+                                title: "Thất bại",
+                                text: `Cập nhật nhà cung cấp "${$scope.editingSupplier.supplierId}" thất bại`,
+                            });
+                        });
+                } else {
+                    // Nếu người dùng chọn "Hủy", bạn có thể thực hiện các hành động tùy ý hoặc không thực hiện gì cả.
+                    // Ví dụ: không thực hiện cập nhật và trở lại biểu mẫu.
+                }
+            });
         } else {
             var url = `${host}`;
             $http.post(url, formData, {
@@ -194,7 +329,7 @@ function SuppliersController($scope, $location, $routeParams, $http) {
                     Swal.fire({
                         icon: "success",
                         title: "Thành công",
-                        text: `Thêm nhà cung cấp ${supplierId} `,
+                        text: `Thêm nhà cung cấp "${supplierId}" thành công `,
                     });
                     $scope.clearImage(); // Xóa ảnh đại diện sau khi thêm
                 })
@@ -204,14 +339,14 @@ function SuppliersController($scope, $location, $routeParams, $http) {
                         Swal.fire({
                             icon: "error",
                             title: "Thất bại",
-                            text: `Thêm nhà cung cấp ${supplierId} thất bại`,
+                            text: `Thêm nhà cung cấp "${supplierId}" thất bại`,
                         });
                     }
                 });
         }
     };
 
-    // Chỉnh sửa thông tin nhà xuất bản và chuyển hướng
+    // Chỉnh sửa thông tin nhà cung cấp và chuyển hướng
     $scope.editSupplierAndRedirect = function (supplierId) {
         var url = `${host}/${supplierId}`;
         $http
@@ -238,35 +373,45 @@ function SuppliersController($scope, $location, $routeParams, $http) {
         $scope.isEditing = true;
     }
 
-    // Xóa nhà xuất bản
+    // Xóa nhà cung cấp
     $scope.deleteSupplier = function (supplierId) {
-        var url = `${host}/${supplierId}`;
-
-        $http
-            .delete(url)
-            .then((resp) => {
-                $scope.loadSuppliers();
-                Swal.fire({
-                    icon: "success",
-                    title: "Thành công",
-                    text: `Xóa nhà cung cấp ${supplierId} thành công`,
-                });
-            })
-            .catch((error) => {
-                if (error.status === 409) {
-                    Swal.fire({
-                        icon: "error",
-                        title: "Thất bại",
-                        text: `Nhà cung cấp mã ${key} đang được sử dụng và không thể xóa.`,
+        Swal.fire({
+            title: 'Xác nhận xóa',
+            text: `Bạn có muốn xóa nhà cung cấp "${supplierId}" không?`,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Xóa',
+            cancelButtonText: 'Hủy',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                var url = `${host}/${supplierId}`;
+                $http
+                    .delete(url)
+                    .then((resp) => {
+                        $scope.loadSuppliers();
+                        Swal.fire({
+                            icon: "success",
+                            title: "Thành công",
+                            text: `Xóa nhà cung cấp "${supplierId}" thành công`,
+                        });
+                    })
+                    .catch((error) => {
+                        if (error.status === 409) {
+                            Swal.fire({
+                                icon: "error",
+                                title: "Thất bại",
+                                text: `Nhà cung cấp mã "${supplierId}" đang được sử dụng và không thể xóa.`,
+                            });
+                        } else {
+                            Swal.fire({
+                                icon: "error",
+                                title: "Thất bại",
+                                text: `Xóa nhà cung cấp "${supplierId}" thất bại`,
+                            });
+                        }
                     });
-                } else {
-                    Swal.fire({
-                        icon: "error",
-                        title: "Thất bại",
-                        text: `Xóa nhà cung cấp ${supplierId} thất bại`,
-                    });
-                }
-            });
+            }
+        });
     };
 
     // Xóa ảnh đại diện và làm mới form
