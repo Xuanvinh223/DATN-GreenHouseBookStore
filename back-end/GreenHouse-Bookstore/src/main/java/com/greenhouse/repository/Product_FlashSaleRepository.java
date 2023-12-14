a551a957ade838037fd366cbfb6d44de6370974f
MinThuc1505 <minhthuc7a1@gmail.com>
2023-12-14 17:04:04 +0700

    Update valid quantity in stock with quantity flash sale

--- back-end/GreenHouse-Bookstore/src/main/java/com/greenhouse/repository/Product_FlashSaleRepository.java
+++ back-end/GreenHouse-Bookstore/src/main/java/com/greenhouse/repository/Product_FlashSaleRepository.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.greenhouse.model.Flash_Sales;
+import com.greenhouse.model.Product_Detail;
 import com.greenhouse.model.Product_Flash_Sale;
 
 public interface Product_FlashSaleRepository extends JpaRepository<Product_Flash_Sale, Integer> {
 
     List<Product_Flash_Sale> findByFlashSaleId(Flash_Sales id);
+
+    Product_Flash_Sale findByFlashSaleIdAndProductDetail(Flash_Sales flashSaleId, Product_Detail pDetail);
    
 
 }
